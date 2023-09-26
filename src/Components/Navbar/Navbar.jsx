6b1ed5fba0cf615bad822a4ae04640a81f2461a2
Rohan <rajputrohan196@gmail.com>
2023-09-26 23:17:22 +0530

    stable version courses chud gaya vapas

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,12 +3,12 @@
 import { useEffect, useState } from 'react';
 import {images} from '../../Constants/images'
 import './Navbarr.css'
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 import { GiHamburgerMenu } from 'react-icons/gi';
 import {GiCrossedBones } from 'react-icons/gi';
 
-import { useState } from 'react';
+// import { useState } from 'react';
 // import { useState } from 'react';
 
 const Navbar = () => {
