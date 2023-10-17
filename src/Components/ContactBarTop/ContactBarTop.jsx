997fe5014ebdaa1cb3f344c51c5cfe7e28b15872
Rohan <rajputrohan196@gmail.com>
2023-10-17 21:49:03 +0530

    New

--- src/Components/ContactBarTop/ContactBarTop.jsx
+++ src/Components/ContactBarTop/ContactBarTop.jsx
@@ -13,19 +13,19 @@ const ContactBarTop = () => {
     <div className="top_bar">
         <div>
           {/* <SocialIcon style={{ height: 10, width: 10 }} url="www.gmail.com" /> */}
-        <h1>contact@savitadevi.com</h1>
+        <h1 className="contactBar_details">contact@savitadevi.com</h1>
         </div>
         <div>
-          <h1>Contact: +91-7424870565</h1>
+          <h1 className="contactBar_details">Contact: +91-7424870565</h1>
         </div>
         <ButtonToolbar> 
-          <Dropdown title="Student Alerts" activeKey="a" className="dropdown_btn"> 
-            <Dropdown.Item eventKey="a">Item A</Dropdown.Item> 
-            <Dropdown.Item eventKey="b">Item B</Dropdown.Item> 
+          <Dropdown title="Student Alerts" activeKey="a"> 
+            <Dropdown.Item eventKey="a">No new alerts</Dropdown.Item> 
+            {/* <Dropdown.Item eventKey="b">Item B</Dropdown.Item> 
             <Dropdown.Item eventKey="c">Item C</Dropdown.Item> 
-            <Dropdown.Item eventKey="d">Item D</Dropdown.Item> 
-          </Dropdown> 
-        </ButtonToolbar>          
+            <Dropdown.Item eventKey="d">Item D</Dropdown.Item>  */}
+            </Dropdown> 
+        </ButtonToolbar>        
     </div>
   )
 }
