eefb189eb64972970333e8c7186185ccc0e3ee73
Chinmay Pathak <pathakchinmay02@gmail.com>
2023-10-17 21:24:09 +0530

    made changes

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { resolve } from 'path';
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import { resolve } from 'path';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({ 
+
+//   resolve: {
+//     alias: {
+//       '@': resolve(__dirname, './src')
+//     },
+//   },
+//   plugins: [react()],
+// })
 
 // https://vitejs.dev/config/
-export default defineConfig({ 
-
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src')
-    },
-  },
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+export default defineConfig({
   plugins: [react()],
-})
+  server: {},
+});
